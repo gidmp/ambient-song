9dfa16eec31b13240827a3817fad321800f4d18a
Daniel Luke Tanoeihusada <danielluke08@gmail.com>
2021-06-04 00:03:55 -0700

    configure connections to spotify api

---
+++ src/utils/spotify.ts
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+const redirectUri = "https://localhost:3000/";
+const clientId = process.env.REACT_APP_API_KEY;
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
